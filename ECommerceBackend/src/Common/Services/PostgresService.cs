c2d20923fd40fb0abfd2b93a43a0bffc4f01bb39
Tomas <tomas.ew88@gmail.com>
2025-01-18 14:48:41 +0200

    Wrapper on Npgsql

--- ECommerceBackend/src/Common/Services/PostgresService.cs
+++ ECommerceBackend/src/Common/Services/PostgresService.cs
@@ -2,7 +2,24 @@ using Npgsql;
 
 namespace ECommerce.Common.Services
 {
-    public class PostgresService
+    public class QueryParameter
+    {
+        public string Key { get; set; }
+        public object Value { get; set; }
+
+        public QueryParameter(string key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+    public interface IPostgresService
+    {
+        public Task<List<object>> ExecuteReaderAsync(string query, QueryParameter[] parameters);
+        public Task<object?> ExecuteScalarAsync(string query, QueryParameter[] parameters);
+    }
+
+    public class PostgresService : IPostgresService
     {
         public NpgsqlConnection Connection { get; }
 
@@ -10,6 +27,37 @@ namespace ECommerce.Common.Services
         {
             Connection = new NpgsqlConnection(connectionString);
             Connection.Open();
+        }
+
+        public async Task<List<object>> ExecuteReaderAsync(string query, QueryParameter[] parameters)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand(query, Connection);
+            foreach (var parameter in parameters)
+            {
+                cmd.Parameters.Add(new NpgsqlParameter(parameter.Key, parameter.Value));
+            }
+            NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
+            List<object> result = new List<object>();
+
+            while (await reader.ReadAsync())
+            {
+                for (int ordinal = 0; ordinal < reader.FieldCount; ordinal++)
+                {
+                    result.Add(reader.GetValue(ordinal));
+                }
+            }
+
+            return result;
+        }
+
+        public async Task<object?> ExecuteScalarAsync(string query, QueryParameter[] parameters)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand(query, Connection);
+            foreach (var parameter in parameters)
+            {
+                cmd.Parameters.Add(new NpgsqlParameter(parameter.Key, parameter.Value));
+            }
+            return await cmd.ExecuteScalarAsync();
         }
     }
 }
