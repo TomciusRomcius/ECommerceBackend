28a842d53c153182f34449bc8449f02d72232b79
Tomas <tomas.ew88@gmail.com>
2025-01-18 14:49:40 +0200

    Auth service test

--- ECommerce.Tests/Services/AuthServiceTest.cs
+++ ECommerce.Tests/Services/AuthServiceTest.cs
@@ -1,48 +1,120 @@
 using ECommerce.Auth;
 using ECommerce.Common.Services;
+using ECommerce.Common.Utils;
+using Microsoft.AspNetCore.Http;
 using Moq;
-using Npgsql;
 using Xunit;
 
 namespace ECommerce.Tests
 {
     public class AuthServiceTests
     {
-        private readonly Mock<PostgresService> _mockPostgresService;
-        private readonly Mock<JwtService> _mockJwtService;
+        private readonly Mock<IPostgresService> _mockPostgresService;
+        private readonly Mock<IJwtService> _mockJwtService;
         private readonly AuthService _authService;
 
         public AuthServiceTests()
         {
-            _mockPostgresService = new Mock<PostgresService>();
-            _mockJwtService = new Mock<JwtService>();
+            _mockPostgresService = new Mock<IPostgresService>();
+            _mockJwtService = new Mock<IJwtService>();
             _authService = new AuthService(_mockPostgresService.Object, _mockJwtService.Object);
         }
 
         [Fact]
         public async Task SignUpWithPassword_ShouldReturnJwtTokenAndId_WhenUserIsCreated()
         {
-            string email = "email@gmail.com";
-            string name = "Name";
-            string lastname = "Lastname";
-            string password = "new-password";
-
             var dto = new SignUpWithPasswordRequestDto()
             {
-                Email = email,
-                Name = name,
-                Lastname = lastname,
-                Password = password
+                Email = "email@gmail.com",
+                Name = "Name",
+                Lastname = "Lastname",
+                Password = "new-password"
             };
 
-            var mockCommand = new Mock<NpgsqlCommand>();
-            mockCommand.Setup(cmd => cmd.ExecuteScalarAsync()).ReturnsAsync(0);
-            _mockJwtService.Setup(jwt => jwt.CreateUserToken(It.IsAny<int>(), It.IsAny<string>())).Returns("token");
+            int expectedId = 0;
+            string expectedToken = "jwt";
+
+            _mockPostgresService.Setup(postgres => postgres.ExecuteScalarAsync(It.IsAny<string>(), It.IsAny<QueryParameter[]>())).ReturnsAsync(expectedId);
+            _mockJwtService.Setup(jwt => jwt.CreateUserToken(It.IsAny<int>(), It.IsAny<string>())).Returns(expectedToken);
 
             AuthResponseDto resDto = await _authService.SignUpWithPassword(dto);
 
-            Assert.Equal(resDto.userId, 0);
-            Assert.Equal(resDto.jwtToken, "token");
+            Assert.Equal(resDto.userId, expectedId);
+            Assert.Equal(resDto.jwtToken, expectedToken);
+        }
+
+        [Fact]
+        public async Task SignUpWithPassword_ShouldReturnThrowError_IfIdIsNull()
+        {
+            var dto = new SignUpWithPasswordRequestDto()
+            {
+                Email = "email@gmail.com",
+                Name = "Name",
+                Lastname = "Lastname",
+                Password = "new-password"
+            };
+
+            _mockPostgresService.Setup(postgres => postgres.ExecuteScalarAsync(It.IsAny<string>(), It.IsAny<QueryParameter[]>())).ReturnsAsync(null);
+            _mockJwtService.Setup(jwt => jwt.CreateUserToken(It.IsAny<int>(), It.IsAny<string>())).Returns("");
+
+            await Assert.ThrowsAnyAsync<Exception>(() => _authService.SignUpWithPassword(dto));
+        }
+
+        [Fact]
+        public async Task SignInWithPassword_ShouldReturnJwtTokenAndId_WhenSuccesful()
+        {
+            string password = "CoolPass";
+            string hashedPassword = PasswordHasher.Hash("CoolPass");
+
+            var dto = new SignInWithPasswordRequestDto()
+            {
+                Email = "email@gmail.com",
+                Password = password
+            };
+
+            string expectedJwt = "jwt";
+            int expectedUserId = 5;
+            var readList = new List<object>()
+            {
+                expectedUserId,
+                hashedPassword
+            };
+
+
+            _mockPostgresService.Setup(postgres => postgres.ExecuteReaderAsync(It.IsAny<string>(), It.IsAny<QueryParameter[]>())).ReturnsAsync(readList);
+            _mockJwtService.Setup(jwt => jwt.CreateUserToken(It.IsAny<int>(), It.IsAny<string>())).Returns(expectedJwt);
+
+            AuthResponseDto result = await _authService.SignInWithPassword(dto);
+
+            Assert.Equal(result.jwtToken, expectedJwt);
+            Assert.Equal(result.userId, expectedUserId);
+        }
+
+        [Fact]
+        public async Task SignInWithPassword_ShouldReturnInvalidPassword_WhenThePasswordIsIncorrect()
+        {
+            string hashedRealPassword = PasswordHasher.Hash("CoolPass1");
+            string givenPassword = "CoolPass";
+
+            var dto = new SignInWithPasswordRequestDto()
+            {
+                Email = "email@gmail.com",
+                Password = givenPassword
+            };
+
+            string expectedJwt = "jwt";
+            int expectedUserId = 5;
+            var readList = new List<object>()
+
+            {
+                expectedUserId,
+                hashedRealPassword
+            };
+
+
+            _mockPostgresService.Setup(postgres => postgres.ExecuteReaderAsync(It.IsAny<string>(), It.IsAny<QueryParameter[]>())).ReturnsAsync(readList);
+            _mockJwtService.Setup(jwt => jwt.CreateUserToken(It.IsAny<int>(), It.IsAny<string>())).Returns(expectedJwt);
+            await Assert.ThrowsAsync<BadHttpRequestException>(() => _authService.SignInWithPassword(dto));
         }
     }
 }
