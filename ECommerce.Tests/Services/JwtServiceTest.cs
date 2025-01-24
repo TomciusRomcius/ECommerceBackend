34a95340a4106aedd15373b018e27d6a4b8ec501
Tomas <tomas.ew88@gmail.com>
2025-01-24 10:32:22 +0200

    Use JwtOptions

--- ECommerce.Tests/Services/JwtServiceTest.cs
+++ ECommerce.Tests/Services/JwtServiceTest.cs
@@ -1,17 +1,23 @@
 using ECommerce.Common.Services;
+using ECommerce.Common.Utils;
 using Xunit;
 
 namespace ECommerce.Tests
 {
     public class JwtServiceTest
     {
+        private JwtOptions _jwtOptions = new JwtOptions(
+            "app",
+            "8E181A76C50E956FB62F3B620945D3BA75DF1E0A3A1B8C34CDD72FFB562379C4",
+            1
+        );
+
         [Fact]
         public void JwtService_ShouldCreateJwtTokens()
         {
-            string key = "8E181A76C50E956FB62F3B620945D3BA75DF1E0A3A1B8C34CDD72FFB562379C4";
             int jwtPartsCount = 3;
 
-            var jwtService = new JwtService(key);
+            var jwtService = new JwtService(_jwtOptions);
             string result = jwtService.CreateUserToken(5, "email");
             string[] jwtParts = result.Split('.');
 
@@ -22,15 +28,25 @@ namespace ECommerce.Tests
         [Fact]
         public void JwtServiceConstructor_ShouldThrowAnError_IfSecretKeyIsInvalid()
         {
-            Assert.ThrowsAny<Exception>(() => new JwtService("abc"));
-            Assert.ThrowsAny<Exception>(() => new JwtService(""));
+            JwtOptions options1 = new JwtOptions(
+                "app",
+                "abc",
+                1
+            );
+
+            JwtOptions options2 = new JwtOptions(
+                "app",
+                "abc",
+                1
+            );
+            Assert.ThrowsAny<Exception>(() => new JwtService(options1));
+            Assert.ThrowsAny<Exception>(() => new JwtService(options2));
         }
 
         [Fact]
         public void JwtServiceVerify_ShouldReadJwtToken_IfTheJwtTokenIsValid()
         {
-            string key = "8E181A76C50E956FB62F3B620945D3BA75DF1E0A3A1B8C34CDD72FFB562379C4";
-            var jwtService = new JwtService(key);
+            var jwtService = new JwtService(_jwtOptions);
 
             string result = jwtService.CreateUserToken(5, "email");
             string jwt = jwtService.CreateUserToken(5, "email");
@@ -44,8 +60,7 @@ namespace ECommerce.Tests
         public void JwtServiceVerify_ShouldThrowAnError_IfTheJwtTokenHasBeenTamperedWith()
         {
             // TODO, convert payload to json and modify prop and pass it to Read.
-            string key = "8E181A76C50E956FB62F3B620945D3BA75DF1E0A3A1B8C34CDD72FFB562379C4";
-            var jwtService = new JwtService(key);
+            var jwtService = new JwtService(_jwtOptions);
 
             string result = jwtService.CreateUserToken(5, "email");
             string jwt = jwtService.CreateUserToken(5, "email");
