39a0b32856fef0fc03b48f5c34ce3b2b298d02d8
Tomas <tomas.ew88@gmail.com>
2025-01-29 12:09:06 +0200

    Allow for null values

--- DataAccess/Utils/QueryParameter.cs
+++ DataAccess/Utils/QueryParameter.cs
@@ -3,14 +3,14 @@ namespace ECommerce.DataAccess.Utils
     public class QueryParameter
     {
         public string? Key { get; set; } = null;
-        public object Value { get; set; }
+        public object? Value { get; set; }
 
-        public QueryParameter(object value)
+        public QueryParameter(object? value)
         {
             Value = value;
         }
 
-        public QueryParameter(string key, object value)
+        public QueryParameter(string key, object? value)
         {
             Key = key;
             Value = value;
