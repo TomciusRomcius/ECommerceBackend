521cb48940d7ca1164727449e3fa26cfadb722ce
Tomas <tomas.ew88@gmail.com>
2025-02-07 15:22:41 +0200

    Move test code to one api route.

--- ECommerceBackend/src/Order/OrderController.cs
+++ ECommerceBackend/src/Order/OrderController.cs
@@ -1,8 +1,12 @@
 using System.Security.Claims;
+using System.Web;
 using ECommerce.Cart;
+using ECommerce.DataAccess.Entities.PaymentSession;
+using ECommerce.DataAccess.Repositories.PaymentSession;
 using ECommerce.PaymentSession;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace ECommerce.Order
 {
@@ -10,17 +14,19 @@ namespace ECommerce.Order
     [Route("[controller]")]
     public class OrderController : ControllerBase
     {
+        readonly IPaymentSessionRepository _paymentSessionRepository;
         readonly ICartService _cartService;
         readonly IStripeSessionService _stripeSessionService;
         readonly IWebHostEnvironment _webHostEnvironment;
         readonly ILogger _logger;
 
-        public OrderController(IStripeSessionService stripeSessionService, ICartService cartService, IWebHostEnvironment webHostEnvironment, ILogger logger)
+        public OrderController(IStripeSessionService stripeSessionService, ICartService cartService, IWebHostEnvironment webHostEnvironment, ILogger logger, IPaymentSessionRepository paymentSessionRepository)
         {
             _stripeSessionService = stripeSessionService;
             _cartService = cartService;
             _webHostEnvironment = webHostEnvironment;
             _logger = logger;
+            _paymentSessionRepository = paymentSessionRepository;
         }
 
         [HttpGet("session")]
@@ -31,40 +37,14 @@ namespace ECommerce.Order
         }
 
         [HttpPost("session")]
-        public async Task<IActionResult> CreateOrderPaymentSession()
+        public async Task<IActionResult> CreateOrderPaymentSession(
+            [FromQuery(Name = "testcharge")] bool testCharge
+        )
         {
-            string? userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (userId is null)
-            {
-                return new UnauthorizedObjectResult("You must be logged in to create payment session!");
-            }
-
-            var items = await _cartService.GetAllUserItems(userId);
-
-            if (items.Count == 0)
-            {
-                // Return invalid operation
-                return BadRequest("There must be at least one cart item to be able to create a payment session");
-            }
-
-            double finalPrice = 0;
-            items.ForEach(item => finalPrice += item.Price * item.Quantity);
-
-            var session = _stripeSessionService.GeneratePaymentSession(
-                new() { UserId = userId, Price = (int)(finalPrice * 100.0) }
-            );
-
-            return Created(nameof(CreateOrderPaymentSession), session);
-        }
-
-        // Creates a session and instantly confirms test payment.
-        [HttpPost("session/test-confirm")]
-        public async Task<IActionResult> CreateOrderPaymentSessionAndConfirm()
-        {
-            if (!_webHostEnvironment.IsDevelopment())
+            if (testCharge && !_webHostEnvironment.IsDevelopment())
             {
                 _logger.LogWarning("Trying to call order/session/test-confirm on a non-development environment");
-                return NotFound();
+                return BadRequest();
             }
 
             string? userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
@@ -73,12 +53,16 @@ namespace ECommerce.Order
                 return new UnauthorizedObjectResult("You must be logged in to create payment session!");
             }
 
+            if (await _paymentSessionRepository.GetPaymentSession(new Guid(userId)) is not null)
+            {
+                return BadRequest("Cannot create a new payment session because there is a pending one!");
+            }
+
             var items = await _cartService.GetAllUserItems(userId);
 
             if (items.Count == 0)
             {
-                // Return invalid operation
-                return BadRequest("There must be at least one cart item to be able to create a payment session");
+                return BadRequest("There must be at least one cart item to be able to create a payment session!");
             }
 
             double finalPrice = 0;
@@ -86,9 +70,25 @@ namespace ECommerce.Order
 
             if (finalPrice > 0)
             {
-                _stripeSessionService.GeneratePaymentSessionAndConfirm(
-                    new() { UserId = userId, Price = (int)(finalPrice * 100.0) }
-                );
+                if (!testCharge)
+                {
+                    _logger.LogInformation("Testing charge webhook");
+
+                    PaymentIntent intent = _stripeSessionService.GeneratePaymentSession(
+                        new() { UserId = userId, Price = (int)(finalPrice * 100.0) }
+                    );
+
+                    await _paymentSessionRepository.CreatePaymentSessionAsync(
+                        new PaymentSessionEntity(intent.Id, new Guid(userId), "stripe")
+                    );
+                }
+
+                else
+                {
+                    _stripeSessionService.GeneratePaymentSessionAndConfirm(
+                        new() { UserId = userId, Price = (int)(finalPrice * 100.0) }
+                    );
+                }
 
                 return Ok();
             }
