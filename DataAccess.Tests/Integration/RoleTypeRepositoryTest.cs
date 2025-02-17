b2c81455fdb23ed8eaabdd20136a4426d2a17665
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-02-17 17:40:45 +0200

    RoleTypeRepository integration test

---
+++ DataAccess.Tests/Integration/RoleTypeRepositoryTest.cs
@@ -0,0 +1,26 @@
+using DataAccess.Test.Integration.Utils;
+using ECommerce.DataAccess.Models.RoleType;
+using ECommerce.DataAccess.Repositories;
+
+namespace DataAccess.Tests.Integration
+{
+    public class RoleTypeRepositoryIntegrationTest
+    {
+        [Fact]
+        public async Task ShouldSuccesfullyCreateAndRetrieveRoleType()
+        {
+            var testContainer = new TestDatabase();
+            var roleTypeRepository = new RoleTypeRepository(testContainer._postgresService);
+
+            string roleName = "Test role name";
+            await roleTypeRepository.CreateAsync(new CreateRoleTypeModel(roleName));
+
+            var retrieved = await roleTypeRepository.FindByNameAsync(roleName);
+
+            Assert.NotNull(retrieved);
+            Assert.Equal(roleName, retrieved.Name);
+
+            await testContainer.DisposeAsync();
+        }
+    }
+}
