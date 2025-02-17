908e1ac40d2ef049d252b51540d718e7eacc60c3
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-02-17 17:45:53 +0200

    Clean up shipping address test

--- DataAccess.Tests/Integration/ShippingAddressRepositoryTest.cs
+++ DataAccess.Tests/Integration/ShippingAddressRepositoryTest.cs
@@ -9,54 +9,31 @@ using Moq;
 
 namespace DataAccess.Tests.Integration
 {
-    public class DataFixture : IAsyncLifetime
+    public class ShippingAddressRepositoryTest
     {
-        public TestDatabase _testContainer { get; }
-        public ShippingAddressRepository? _shippingAddressRepository { get; }
-        public Guid _userId { get; set; } = new Guid();
-        public ShippingAddressModel? _address { get; set; } = null;
-
-        public DataFixture()
-        {
-            _testContainer = new TestDatabase();
-            _shippingAddressRepository = new ShippingAddressRepository(_testContainer._postgresService);
-        }
-
-        public async Task InitializeAsync()
-        {
-            var userRepository = new UserRepository(
-                _testContainer._postgresService,
-                new Mock<ILogger>().Object
-            );
-
-            // Create base user to whom we are going to attach a shipping address
-            await userRepository.CreateAsync(
-                new UserModel(_userId.ToString(), "email@gmail.com", "passwordhash", "firstname", "lastname")
-            );
-        }
-
-        public async Task DisposeAsync()
-        {
-            await _testContainer.DisposeAsync();
-        }
-    }
-
-    [Collection("Shipping address creation flow")]
-    public class ShippingAddressRepositoryTest : IClassFixture<DataFixture>
-    {
-        readonly DataFixture _dataFixture;
-        public ShippingAddressRepositoryTest(DataFixture dataFixture)
-        {
-            _dataFixture = dataFixture;
-        }
-
         [Fact]
         public async Task ShouldSuccesfullyCreateAndRetrieveTheShippingAddress()
         {
+            var testContainer = new TestDatabase();
 
-            _dataFixture._address = new ShippingAddressModel
+            // Create base user to whom we are going to attach a shipping address
+            var userRepository = new UserRepository(
+                testContainer._postgresService,
+                new Mock<ILogger>().Object
+            );
+
+            var userId = new Guid();
+
+            await userRepository.CreateAsync(
+                new UserModel(userId.ToString(), "email@gmail.com", "passwordhash", "firstname", "lastname")
+            );
+
+            // Create a shipping address
+            var shippingAddressRepository = new ShippingAddressRepository(testContainer._postgresService);
+
+            var shippingAddress = new ShippingAddressModel
             {
-                UserId = _dataFixture._userId.ToString(),
+                UserId = userId.ToString(),
                 RecipientName = "RecipientName",
                 StreetAddress = "StreetAddress",
                 ApartmentUnit = "ApartmentUnit",
@@ -67,25 +44,20 @@ namespace DataAccess.Tests.Integration
                 MobileNumber = "Mobile",
             };
 
-            await _dataFixture._shippingAddressRepository.AddAddressAsync(_dataFixture._address);
+            await shippingAddressRepository.AddAddressAsync(shippingAddress);
 
-            if (_dataFixture._address is null)
-            {
-                throw new DataException("Shipping address is null! Cannot proceed with the test");
-            }
+            var addresses = await shippingAddressRepository.GetAddresses(userId.ToString());
+            var retrieved = addresses.Where(item => item.ShippingAddressId == shippingAddress.ShippingAddressId).ToList()[0];
 
-            var addresses = await _dataFixture._shippingAddressRepository.GetAddresses(_dataFixture._userId.ToString());
-            var result = addresses.Where(item => item.ShippingAddressId == _dataFixture._address.ShippingAddressId).ToList()[0];
-
-            Assert.Equal(_dataFixture._address.UserId, result.UserId);
-            Assert.Equal(_dataFixture._address.RecipientName, result.RecipientName);
-            Assert.Equal(_dataFixture._address.StreetAddress, result.StreetAddress);
-            Assert.Equal(_dataFixture._address.ApartmentUnit, result.ApartmentUnit);
-            Assert.Equal(_dataFixture._address.City, result.City);
-            Assert.Equal(_dataFixture._address.State, result.State);
-            Assert.Equal(_dataFixture._address.PostalCode, result.PostalCode);
-            Assert.Equal(_dataFixture._address.Country, result.Country);
-            Assert.Equal(_dataFixture._address.MobileNumber, result.MobileNumber);
+            Assert.Equal(shippingAddress.UserId, retrieved.UserId);
+            Assert.Equal(shippingAddress.RecipientName, retrieved.RecipientName);
+            Assert.Equal(shippingAddress.StreetAddress, retrieved.StreetAddress);
+            Assert.Equal(shippingAddress.ApartmentUnit, retrieved.ApartmentUnit);
+            Assert.Equal(shippingAddress.City, retrieved.City);
+            Assert.Equal(shippingAddress.State, retrieved.State);
+            Assert.Equal(shippingAddress.PostalCode, retrieved.PostalCode);
+            Assert.Equal(shippingAddress.Country, retrieved.Country);
+            Assert.Equal(shippingAddress.MobileNumber, retrieved.MobileNumber);
         }
     }
 }
