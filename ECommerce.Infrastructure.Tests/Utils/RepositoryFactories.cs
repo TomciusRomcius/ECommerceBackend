3830eab26aa44897de353ec1325c4e8e5ddf34bb
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-06-08 11:45:28 +0300

    Utility class for making repository initialization easier

---
+++ ECommerce.Infrastructure.Tests/Utils/RepositoryFactories.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Domain.Validators.Product;
+using ECommerce.Domain.Validators.User;
+using ECommerce.Infrastructure.Repositories;
+using ECommerce.Infrastructure.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ECommerce.Infrastructure.Tests.Utils
+{
+    public static class RepositoryFactories
+    {
+        public static UserRepository CreateUserRepository(IPostgresService postgresService) =>
+            new UserRepository(
+                postgresService,
+                new Mock<ILogger<UserRepository>>().Object,
+                new UserEntityValidator(),
+                new UpdateUserModelValidator()
+            );
+
+        public static ProductRepository CreateProductRepository(IPostgresService postgresService) =>
+            new ProductRepository(
+                postgresService,
+                new ProductEntityValidator(),
+                new UpdateProductModelValidator()
+            );
+    }
+}
