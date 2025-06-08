5f31375d0e7d59e986dd48752e2927d56dd397c2
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-06-08 09:52:07 +0300

    Utility functions for converting validation errors to result errors

---
+++ ECommerce.Domain/Utils/ResultUtils.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation.Results;
+
+namespace ECommerce.Domain.Utils
+{
+    public static class ResultUtils
+    {
+        public static List<ResultError> ValidationFailuresToResultErrors(List<ValidationFailure> errors)
+        {
+            return errors.Select(
+                error => new ResultError(ResultErrorType.VALIDATION_ERROR, error.ErrorMessage)
+            ).ToList();
+        }
+
+        /// <summary>
+        /// Errors list must not be empty, otherwise an exception will be thrown
+        /// </summary>
+        public static ResultError ValidationFailuresToResultError(List<ValidationFailure> errors)
+        {
+            return new ResultError(ResultErrorType.VALIDATION_ERROR, errors.First().ErrorMessage);
+        }
+    }
+}
