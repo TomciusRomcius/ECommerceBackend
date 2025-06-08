df185847353eb2c8ddc38e66f64d9ba7f573cfdc
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-06-08 09:25:42 +0300

    Implement category validation

---
+++ ECommerce.Domain/Validators/Category/CategoryValidationRules.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace ECommerce.Domain.Validators.Category
+{
+    public static class CategoryValidationRules
+    {
+        public static IRuleBuilderOptions<T, string?> IsCategoryName<T>(
+            this IRuleBuilder<T, string?> ruleBuilder) where T : class
+        {
+            return ruleBuilder
+                .NotEmpty().WithMessage("Category name cannot be empty!")
+                .MinimumLength(3).WithMessage("Category name be atleast 3 characters long!")
+                .MaximumLength(20).WithMessage("Category name length must not exceed 20 characters!");
+        }
+    }
+}
---
+++ ECommerce.Domain/Validators/Category/CategoryValidators.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Models;
+using FluentValidation;
+
+namespace ECommerce.Domain.Validators.Category
+{
+    public class CategoryEntityValidators : AbstractValidator<CategoryEntity>
+    {
+        public CategoryEntityValidators()
+        {
+            RuleFor(x => x.Name).IsCategoryName();
+        }
+    }
+
+    public class UpdateCategoryModelValidator : AbstractValidator<UpdateCategoryModel>
+    {
+        public UpdateCategoryModelValidator()
+        {
+            RuleFor(x => x.Name).IsCategoryName().When(x => x.Name != null);
+        }
+    }
+}
