37aa90925da84e3cdff292277d1e59c8fed07bc7
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-06-08 09:29:33 +0300

    Implement validation for store location entity and update model

---
+++ ECommerce.Domain/Validators/StoreLocation/StoreLocationValidationRules.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace ECommerce.Domain.Validators.Product
+{
+    public static class StoreLocationValidationRules
+    {
+        public static IRuleBuilderOptions<T, string?> IsStoreLocationDisplayName<T>(
+            this IRuleBuilder<T, string?> ruleBuilder
+        ) where T : class
+        {
+            return ruleBuilder.NotEmpty().WithMessage("StoreLocation display name cannot be empty!")
+                .MinimumLength(8).WithMessage("StoreLocation display name must be atleast 4 characters long!")
+                .MaximumLength(60).WithMessage("StoreLocation display name length must not exceed 60 characters!");
+        }
+
+        public static IRuleBuilderOptions<T, string?> IsStoreLocationAddress<T>(
+            this IRuleBuilder<T, string?> ruleBuilder
+        ) where T : class
+        {
+            return ruleBuilder.NotEmpty().WithMessage("StoreLocation display name cannot be empty!")
+                .MinimumLength(8).WithMessage("StoreLocation display name must be atleast 4 characters long!")
+                .MaximumLength(60).WithMessage("StoreLocation display name length must not exceed 60 characters!");
+        }
+    }
+}
---
+++ ECommerce.Domain/Validators/StoreLocation/StoreLocationValidators.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Models;
+using FluentValidation;
+
+namespace ECommerce.Domain.Validators.Product
+{
+    public class StoreLocationEntityValidation : AbstractValidator<StoreLocationEntity>
+    {
+        public StoreLocationEntityValidation()
+        {
+            RuleFor(x => x.DisplayName).IsStoreLocationDisplayName();
+            RuleFor(x => x.Address).IsStoreLocationAddress();
+        }
+    }
+
+    public class UpdateStoreLocationModelValidation : AbstractValidator<UpdateStoreLocationModel>
+    {
+        public UpdateStoreLocationModelValidation()
+        {
+            RuleFor(x => x.DisplayName)
+                .IsStoreLocationDisplayName()
+                .When(x => x.DisplayName != null);
+            RuleFor(x => x.Address)
+                .IsStoreLocationAddress()
+                .When(x => x.Address != null);
+        }
+    }
+
+}
