60f7f93f45eb9f1efac71d1e99442a7be07459ed
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-06-08 08:44:55 +0300

    Product validation

---
+++ ECommerce.Domain/Validators/Product/ProductEntityValidator.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Domain.Entities;
+using FluentValidation;
+
+namespace ECommerce.Domain.Validators.Product
+{
+    public class ProductEntityValidator : AbstractValidator<ProductEntity>
+    {
+        public ProductEntityValidator()
+        {
+            RuleFor(x => x.Name).IsProductName();
+            RuleFor(x => x.Description).IsProductDescription();
+            RuleFor(x => x.Price).IsProductPrice();
+        }
+    }
+}
