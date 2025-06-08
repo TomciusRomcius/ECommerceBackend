136f421caa777dc09c705a9d8353482b1fcdfee4
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-06-08 11:14:12 +0300

    Utility function for converting result error to an error http response

---
+++ ECommerce.Presentation/src/Utils/ControllerUtils.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Domain.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Presentation.src.Utils
+{
+    public static class ControllerUtils
+    {
+        public static IActionResult ResultErrorToResponse(ResultError error)
+        {
+            int statusCode = ResultErrorTypeToStatusCode(error.ErrorType);
+
+            ProblemDetails problemDetails = new ProblemDetails()
+            {
+                Title = error.Message,
+                Detail = error.Message,
+                Status = statusCode
+            };
+
+            return new ObjectResult(error)
+            {
+                StatusCode = statusCode
+            };
+        }
+
+        private static int ResultErrorTypeToStatusCode(ResultErrorType errorType)
+        {
+            return errorType switch
+            {
+                ResultErrorType.UNKNOWN_ERROR => 500,
+                ResultErrorType.VALIDATION_ERROR => 403,
+                ResultErrorType.INVALID_OPERATION_ERROR => 403,
+                ResultErrorType.UNAUTHORIZED => 401,
+                _ => 500,
+            };
+        }
+    }
+}
