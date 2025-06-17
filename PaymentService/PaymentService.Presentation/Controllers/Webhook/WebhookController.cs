54a50aabfef42fbb447dc5113540cf45f6bc7ca0
Tomas <tomas.ew88@gmail.com>
2025-06-17 14:42:27 +0300

    Webhook controller

--- PaymentService/PaymentService.Presentation/Controllers/Webhook/WebhookController.cs
+++ PaymentService/PaymentService.Presentation/Controllers/Webhook/WebhookController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PaymentService.Application.Interfaces;
 
 namespace PaymentService.Presentation.Controllers.Webhook
 {
@@ -7,15 +8,24 @@ namespace PaymentService.Presentation.Controllers.Webhook
     public class WebhookController : ControllerBase
     {
         private readonly ILogger<WebhookController> _logger;
+        private readonly IWebhookCoordinatorService _webhookCoordinator;
 
-        public WebhookController(ILogger<WebhookController> logger)
+        public WebhookController(ILogger<WebhookController> logger, IWebhookCoordinatorService webhookCoordinator)
         {
             _logger = logger;
+            _webhookCoordinator = webhookCoordinator;
         }
 
         [HttpPost("stripe")]
-        public IActionResult StripeWebhook()
+        public async Task<IActionResult> StripeWebhookAsync()
         {
+            string json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            string? signature = HttpContext.Request.Headers["Stripe-Signature"];
+            if (signature == null)
+            {
+                return Unauthorized("Signature not provided");
+            }
+            await _webhookCoordinator.HandlePaymentWebhook(json, signature);
             return Ok();
         }
     }
