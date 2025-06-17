f8b7d931dae52ac9c0174f60dc113ab3b87ac341
Tomas <tomas.ew88@gmail.com>
2025-06-17 14:43:36 +0300

    Produce kafka charge succeeded event

---
+++ PaymentService/PaymentService.Infrastructure/EventTypes/ChargeSucceededEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace PaymentService.Infrastructure.EventTypes
+{
+    internal class ChargeSucceededEvent
+    {
+        public required string UserId { get; set; }
+        public required decimal Ammount { get; set; }
+    }
+}
--- PaymentService/PaymentService.Infrastructure/Services/WebhookStrategies/ChargeSucceededStrategy.cs
+++ PaymentService/PaymentService.Infrastructure/Services/WebhookStrategies/ChargeSucceededStrategy.cs
@@ -1,44 +1,54 @@
-﻿using PaymentService.Domain.Utils;
+﻿using EventSystemHelper.Kafka.Services;
+using EventSystemHelper.Kafka.Utils;
+using PaymentService.Domain.Utils;
+using PaymentService.Infrastructure.EventTypes;
 using PaymentService.Infrastructure.Interfaces;
 using Stripe;
+using System.Text.Json;
 
 namespace PaymentService.Infrastructure.Services.WebhookStrategies
 {
     public class ChargeSucceededStrategy : IStripeWebhookStrategy
     {
-        //IOrderService _orderService;
+        private readonly KafkaConfiguration _kafkaConfiguration;
 
-        //public ChargeSucceededStrategy(IOrderService orderService)
-        //{
-        //    _orderService = orderService;
-        //}
+        public ChargeSucceededStrategy(KafkaConfiguration kafkaConfiguration)
+        {
+            _kafkaConfiguration = kafkaConfiguration;
+        }
+
+        public string EventType => "charge.succeeded";
 
         public async Task<ResultError?> RunAsync(IHasObject ev)
         {
-            //    if (ev is not Charge)
-            //    {
-            //        return new ResultError(
-            //            ResultErrorType.INVALID_OPERATION_ERROR,
-            //            "Event is not of type Charge!"
-            //        );
-            //    }
-            //    var charge = ev as Charge;
+            KafkaEventProducer producer = new KafkaEventProducer(_kafkaConfiguration);
+            var stripeEvent = ev as Charge;
 
-            //    if (charge is null)
-            //    {
-            //        return new ResultError(ResultErrorType.VALIDATION_ERROR, "Charge object is null!");
-            //    }
-            //    charge.Metadata.TryGetValue("userid", out string? userId);
-            //    if (userId == null)
-            //    {
-            //        return new ResultError(
-            //            ResultErrorType.VALIDATION_ERROR,
-            //            "Charge event metadata does not have an user id attatched to it"
-            //        );
-            //    }
+            if (stripeEvent is not Charge)
+            {
+                return new ResultError(
+                    ResultErrorType.INVALID_OPERATION_ERROR,
+                    "Trying to run a charge succeeded strategy when given object is not a charge!"
+                );
+            }
+            stripeEvent.Metadata.TryGetValue("userid", out string? userId);
+            if (String.IsNullOrWhiteSpace(userId))
+            {
+                return new ResultError(
+                    ResultErrorType.INVALID_OPERATION_ERROR,
+                    "Charge object does not have an userid attatched to its metadata!"
+                );
+            }
 
-            //    await _orderService.OnCharge(new Guid(userId));
+            var kafkaEvent = new ChargeSucceededEvent
+            {
+                UserId = userId,
+                Ammount = stripeEvent.Amount,
+            };
 
+            var jsonMessage = JsonSerializer.Serialize(kafkaEvent);
+
+            await producer.ProduceEventAsync("charge-succeeded", jsonMessage, CancellationToken.None);
             return null;
         }
     }
