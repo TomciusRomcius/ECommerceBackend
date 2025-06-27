cec5f4436f03fdb323d212324d9d9f0c089cb4de
Tomas Rimkevicius <tomas.ew88@gmail.com>
2025-06-27 15:41:38 +0300

    Setup Postgres and Main API with Helm

---
+++ scripts/setup-env.sh
@@ -0,0 +1,7 @@
+cd ../
+echo "Database__Host=postgres" > .env 
+echo "Database__Port=5432" >> .env 
+echo "Database__Username=postgres" >> .env 
+echo "Database__Database=postgres" >> .env 
+echo "Master__Email=masteruser@gmail.com" >> .env
+echo "Master__Password=Masterpassword.55" >> .env
