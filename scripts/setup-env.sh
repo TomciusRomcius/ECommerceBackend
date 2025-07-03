d92de3171b7d7081b89a49680525437d8d543a1a
Tomas <tomas.ew88@gmail.com>
2025-07-03 10:29:22 +0300

    Add db password fields

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,16 +1,18 @@
 cd ../
 echo "MainApi__Database__Host=postgres" > .env 
 echo "MainApi__Database__Port=5432" >> .env 
-echo "MainApi__Database__Username=postgres" >> .env 
 echo "MainApi__Database__Database=postgres" >> .env
+echo "MainApi__Database__Username=postgres" >> .env 
+echo "MainAPi__Database__Password=postgres" >> .env 
 
 echo "MainApi__Master__Email=masteruser@gmail.com" >> .env
 echo "MainApi__Master__Password=Masterpassword.55" >> .env
 
 echo "PaymentService__Database__Host=postgres" >> .env 
 echo "PaymentService__Database__Port=5432" >> .env 
-echo "PaymentService__Database__Username=postgres" >> .env 
 echo "PaymentService__Database__Database=postgres" >> .env 
+echo "PaymentService__Database__Username=postgres" >> .env 
+echo "PaymentService__Database__Password=postgres" >> .env 
 
 read -p "Enter Stripe API key(or leave empty): " stripeApiKey
 read -p "Enter Stripe webhook secret(or leave empty): " stripeWebhookSecret
