628be5ddb66de45c4952fec9ba49a17c105dd122
tomas <tomas.ew88@gmail.com>
2025-10-29 20:40:11 +0200

    Remove mainapi k8 + setup k8 script for microservices

--- scripts/setup-k8-dev.sh
+++ scripts/setup-k8-dev.sh
@@ -5,24 +5,48 @@ cd ..
 
 echo "Bulding docker images"
 
-docker build -t ecommercebackend-main-api:prod ./MainApi
-docker build -t ecommercebackend-main-api-db-migrator:prod --target migrate ./MainApi
+docker build -t ecommercebackend-user-service:prod ./UserService
+docker build -t ecommercebackend-user-service-db-migrator:prod --target migrate ./UserService
+
+docker build -t ecommercebackend-product-service:prod ./ProductService
+docker build -t ecommercebackend-product-service-db-migrator:prod --target migrate ./ProductService
+
+docker build -t ecommercebackend-store-service:prod ./StoreService
+docker build -t ecommercebackend-store-service-db-migrator:prod --target migrate ./StoreService
+
+docker build -t ecommercebackend-order-service:prod ./OrderService
+docker build -t ecommercebackend-order-service-db-migrator:prod --target migrate ./OrderService
 
 docker build -t ecommercebackend-payment-service:prod ./PaymentService
 docker build -t ecommercebackend-payment-service-db-migrator:prod --target migrate ./PaymentService
 
 kind load docker-image \
-  ecommercebackend-main-api:prod \
-  ecommercebackend-main-api-db-migrator:prod \
+  ecommercebackend-user-service:prod \
+  ecommercebackend-user-service-db-migrator:prod \
+  ecommercebackend-product-service:prod \
+  ecommercebackend-product-service-db-migrator:prod \
+  ecommercebackend-store-service:prod \
+  ecommercebackend-store-service-db-migrator:prod \
+  ecommercebackend-order-service:prod \
+  ecommercebackend-order-service-db-migrator:prod \
   ecommercebackend-payment-service:prod \
   ecommercebackend-payment-service-db-migrator:prod
 
-kubectl create configmap main-api-config \
-  --from-env-file=./MainApi/.env
+kubectl create configmap user-service-config \
+  --from-env-file=./UserService/.env
+kubectl create configmap product-service-config \
+  --from-env-file=./ProductService/.env
+kubectl create configmap store-service-config \
+  --from-env-file=./StoreService/.env
+kubectl create configmap order-service-config \
+  --from-env-file=./OrderService/.env
 kubectl create configmap payment-service-config \
   --from-env-file=./PaymentService/.env
 
-kubectl create secret generic main-api-db-secret --from-literal=Database__Password=POSTGRES_PASSWORD > /dev/null 2>&1
+kubectl create secret generic user-service-db-secret --from-literal=Database__Password=POSTGRES_PASSWORD > /dev/null 2>&1
+kubectl create secret generic product-service-db-secret --from-literal=Database__Password=POSTGRES_PASSWORD > /dev/null 2>&1
+kubectl create secret generic store-service-db-secret --from-literal=Database__Password=POSTGRES_PASSWORD > /dev/null 2>&1
+kubectl create secret generic order-service-db-secret --from-literal=Database__Password=POSTGRES_PASSWORD > /dev/null 2>&1
 kubectl create secret generic payment-service-db-secret --from-literal=Database__Password=POSTGRES_PASSWORD > /dev/null 2>&1
 
 read -p "Enter Stripe API Key: " stripeApiKey
@@ -32,6 +56,10 @@ echo "Stripe__ApiKey=$stripeApiKey" > .stripe
 echo "Stripe__WebhookSecret=$stripeWebhookSecret" >> .stripe
 
 kubectl create secret generic stripe-secret --from-env-file=.stripe
+
+kubectl create secret generic order-service-db-secret --from-literal=Database__Password=POSTGRES_PASSWORD > /dev/null 2>&1
+kubectl create secret generic payment-service-db-secret --from-literal=Database__Password=POSTGRES_PASSWORD > /dev/null 2>&1
+
 rm .stripe
 
 kubectl create secret generic master-user-secrets \
