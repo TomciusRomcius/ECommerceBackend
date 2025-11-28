082e2093c79500b3337779ccdac15a7fd5662073
tomas <tomas.ew88@gmail.com>
2025-11-28 09:27:45 +0200

    StoreService use Postgres configuration from ECommerceBackend.Utils

--- ProductService/ProductService.Application/src/Persistence/DatabaseContext.cs
+++ ProductService/ProductService.Application/src/Persistence/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ECommerceBackend.Utils.Database;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using ProductService.Domain.Entities;
 
--- ProductService/ProductService.Presentation/src/Program.cs
+++ ProductService/ProductService.Presentation/src/Program.cs
@@ -1,31 +1,16 @@
-using System.Data;
-using Microsoft.Extensions.Options;
 using ProductService.Application;
 using ProductService.Application.Persistence;
 using ProductService.Application.Services;
+using ECommerceBackend.Utils.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
-if (builder.Configuration.GetSection("Database").Exists())
-    builder.Services.AddOptions<PostgresConfiguration>()
-        .Bind(builder.Configuration.GetSection("Database"))
-        .ValidateDataAnnotations();
-
-else if (builder.Configuration["ASPNETCORE_ENVIRONMENT"] != "Production")
-    builder.Services.AddSingleton<IOptions<PostgresConfiguration>>(Options.Create<PostgresConfiguration>(
-        new PostgresConfiguration
-        {
-            Host = "postgresas",
-            Database = "postgres",
-            Username = "postgres",
-            Password = "postgres"
-        }));
-
-else
-    throw new DataException("Configuration Database properties are not defined!");
+builder.Services.AddOptions<PostgresConfiguration>()
+    .Bind(builder.Configuration.GetSection("Database"))
+    .ValidateDataAnnotations();
 
 builder.Services.AddDbContext<DatabaseContext>();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(MediatREntryPoint).Assembly));
