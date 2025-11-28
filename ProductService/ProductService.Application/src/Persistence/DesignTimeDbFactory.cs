326c319da505ce66aacf104758e8d04b58eaf6b9
tomas <tomas.ew88@gmail.com>
2025-11-28 09:29:28 +0200

    Product service design time db factory for migration scripts using cli arguments

---
+++ ProductService/ProductService.Application/src/Persistence/DesignTimeDbFactory.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+using ECommerceBackend.Utils.Database;
+
+namespace ProductService.Application.Persistence;
+
+public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+{
+    public DatabaseContext CreateDbContext(string[] args)
+    {
+        PostgresConfiguration cfg = new PostgresConfigurationBuilder().Build(args);
+        return new DatabaseContext(Options.Create(cfg));
+    }
+}
