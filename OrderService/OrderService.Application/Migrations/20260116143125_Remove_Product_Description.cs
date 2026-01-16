1ba39bc708c977e9fbcce4091b0ead84773692a7
Tomas <tomas.ew88@gmail.com>
2026-01-16 18:44:31 +0200

    Remove product description from the order product table

---
+++ OrderService/OrderService.Application/Migrations/20260116143125_Remove_Product_Description.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_Product_Description : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActiveOrders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActiveOrders",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActiveOrders", x => new { x.UserId, x.OrderId });
+                    table.ForeignKey(
+                        name: "FK_ActiveOrders_Orders_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderEntityId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+    }
+}
