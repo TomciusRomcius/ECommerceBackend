85b30c9e2f94abb361a5c3860af482a58f98094d
Tomas <tomas.ew88@gmail.com>
2026-03-13 22:13:15 +0200

    Get store locations BFF

---
+++ BFF/Src/StoreLocations/StoreLocationsController.cs
@@ -0,0 +1,31 @@
+using ECommerceBackend.Utils.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace BFF.StoreLocations;
+
+public class StoreLocationDto
+{
+    public int Id { get; set; }
+    public string DisplayName { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+}
+
+[ApiController]
+[Route("[controller]")]
+public class StoreLocationsController(ILogger<StoreLocationsController> logger, HttpClient httpClient, IOptions<MicroserviceHosts> hosts) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetStoreLocations([FromQuery] int page)
+    {
+        var upstreamUrl = $"{hosts.Value.StoreServiceUrl}/StoreLocation?pageNumber={page}";
+        logger.LogDebug("Fetching store locations from {Url}", upstreamUrl);
+
+        var response = await httpClient.GetAsync(upstreamUrl);
+        response.EnsureSuccessStatusCode();
+
+        var locations = await response.Content.ReadFromJsonAsync<List<StoreLocationDto>>();
+        return Ok(locations);
+    }
+}
+
