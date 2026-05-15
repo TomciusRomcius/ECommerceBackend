7a60a0a0eaae56a9dfbfb6f9fd2e7ea50c11e3fe
Tomas <tomas.ew88@gmail.com>
2026-05-15 15:44:07 +0300

    Add frontend OIDC client

--- terraform/keycloak/dev.tfvars
+++ terraform/keycloak/dev.tfvars
@@ -2,3 +2,14 @@ keycloak_admin_username = "admin"
 keycloak_admin_password = "admin"
 keycloak_url = "http://localhost:8080/auth"
 ecommerce_api_client_secret = "secret"
+
+frontend_root_url = "http://localhost:4200"
+frontend_valid_redirect_uris = [
+  "http://localhost:4200/*",
+]
+frontend_web_origins = [
+  "http://localhost:4200",
+]
+frontend_valid_post_logout_redirect_uris = [
+  "http://localhost:4200/*",
+]
--- terraform/keycloak/main.tf
+++ terraform/keycloak/main.tf
@@ -18,6 +18,36 @@ variable "ecommerce_api_client_secret" {
     description = "Client secret of the ecommerce-api secret"
 }
 
+variable "frontend_root_url" {
+  type        = string
+  description = "Root URL of the Angular frontend (used as Keycloak client root/base URL)"
+  default     = "http://localhost:4200"
+}
+
+variable "frontend_valid_redirect_uris" {
+  type        = list(string)
+  description = "Allowed OAuth redirect URIs for the public frontend client"
+  default = [
+    "http://localhost:4200/*",
+  ]
+}
+
+variable "frontend_web_origins" {
+  type        = list(string)
+  description = "Allowed CORS origins for the public frontend client"
+  default = [
+    "http://localhost:4200",
+  ]
+}
+
+variable "frontend_valid_post_logout_redirect_uris" {
+  type        = list(string)
+  description = "Allowed post-logout redirect URIs for the public frontend client"
+  default = [
+    "http://localhost:4200/*",
+  ]
+}
+
 terraform {
   required_providers {
     keycloak = {
@@ -40,7 +70,7 @@ resource "keycloak_realm" "ecommerce_api" {
   display_name      = "Ecommerce API"
 }
 
-resource "keycloak_openid_client" "openid_client" {
+resource "keycloak_openid_client" "ecommerce-api" {
   realm_id            = keycloak_realm.ecommerce_api.id
   client_id           = "ecommerce-api"
   client_secret = var.ecommerce_api_client_secret
@@ -52,6 +82,37 @@ resource "keycloak_openid_client" "openid_client" {
   service_accounts_enabled = true
 }
 
+resource "keycloak_openid_client" "frontend" {
+  realm_id  = keycloak_realm.ecommerce_api.id
+  client_id = "frontend"
+
+  name    = "Frontend"
+  enabled = true
+
+  access_type               = "PUBLIC"
+  standard_flow_enabled     = true
+  full_scope_allowed        = true
+  use_refresh_tokens        = true
+  pkce_code_challenge_method = "S256"
+
+  root_url = var.frontend_root_url
+  base_url = var.frontend_root_url
+
+  valid_redirect_uris             = var.frontend_valid_redirect_uris
+  web_origins                     = var.frontend_web_origins
+  valid_post_logout_redirect_uris = var.frontend_valid_post_logout_redirect_uris
+}
+
+resource "keycloak_openid_client_default_scopes" "frontend_client_default_scopes" {
+  realm_id  = keycloak_realm.ecommerce_api.id
+  client_id = keycloak_openid_client.frontend.id
+
+  default_scopes = [
+    "profile",
+    "email",
+  ]
+}
+
 resource "keycloak_role" "ecommerce_admin_role" {
   realm_id    = keycloak_realm.ecommerce_api.id
   name        = "ecommerce-admin"
@@ -59,7 +120,7 @@ resource "keycloak_role" "ecommerce_admin_role" {
 
 resource "keycloak_openid_hardcoded_role_protocol_mapper" "hardcoded_admin_role" {
   realm_id  = keycloak_realm.ecommerce_api.id
-  client_id = keycloak_openid_client.openid_client.id
+  client_id = keycloak_openid_client.ecommerce-api.id
   name      = "hardcoded-admin-role"
   
   role_id = keycloak_role.ecommerce_admin_role.id
@@ -68,7 +129,7 @@ resource "keycloak_openid_hardcoded_role_protocol_mapper" "hardcoded_admin_role"
 # Protocol mapper to include realm roles in the token
 resource "keycloak_openid_user_realm_role_protocol_mapper" "realm_roles_mapper" {
   realm_id  = keycloak_realm.ecommerce_api.id
-  client_id = keycloak_openid_client.openid_client.id
+  client_id = keycloak_openid_client.ecommerce-api.id
   name      = "realm-roles-mapper"
   
   claim_name       = "roles"
@@ -85,12 +146,12 @@ resource "keycloak_openid_audience_protocol_mapper" "audience_mapper" {
   realm_id        = keycloak_realm.ecommerce_api.id
   client_scope_id = keycloak_openid_client_scope.audience_client_scope.id
   name            = "audience-mapper"
-  included_client_audience = keycloak_openid_client.openid_client.client_id
+  included_client_audience = keycloak_openid_client.ecommerce-api.client_id
 }
 
 resource "keycloak_openid_client_default_scopes" "ecommerce_api_client_default_scopes" {
   realm_id  = "ecommerce-api"
-  client_id = keycloak_openid_client.openid_client.id
+  client_id = keycloak_openid_client.ecommerce-api.id
 
   default_scopes = [
     "profile",
