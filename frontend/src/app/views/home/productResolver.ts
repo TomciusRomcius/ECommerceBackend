d1a00693e8a8150be962b8277c497e08d655d8f0
Tomas <tomas.ew88@gmail.com>
2026-05-16 12:09:05 +0300

    Fix product resolver by using ResolverFn

--- frontend/src/app/views/home/productResolver.ts
+++ frontend/src/app/views/home/productResolver.ts
@@ -1,14 +1,13 @@
-import { ActivatedRouteSnapshot, MaybeAsync, RedirectCommand, Resolve, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from "@angular/router";
 import ProductModel from "../../models/product-model";
 import { inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
-import { Observable } from "rxjs";
 
-export class ProductResolver implements Resolve<ProductModel[]> {
-  httpClient = inject(HttpClient);
-
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ProductModel[]> {
-    return this.httpClient.get<ProductModel[]>(`${environment.backendApi}/storeproducts`);
-  }
+export const productResolver: ResolveFn<ProductModel[]> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
+) => {
+  const httpClient = inject(HttpClient);
+  return httpClient.get<ProductModel[]>(`${environment.backendApi}/storeproducts`);
 }
