581b5ff90e8003b4af8d2f5d3be9928205781374
Tomas <tomas.ew88@gmail.com>
2026-05-17 23:54:47 +0300

    Add header

--- frontend/src/app/components/page-padder/page-padder.ts
+++ frontend/src/app/components/page-padder/page-padder.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-page-padder',
-  imports: [],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './page-padder.html',
 })
-export class PagePadder {
-
-}
+export class PagePadder {}
